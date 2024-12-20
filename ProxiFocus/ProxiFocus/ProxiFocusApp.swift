e7334a86a1759cf122752fb15c21e368abeb3b42
Daylen Mackey <daylen.j.mackey@gmail.com>
2024-12-19 18:49:19 -0800

    Initial commit

---
+++ ProxiFocus/ProxiFocus/ProxiFocusApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProxiFocusApp.swift
+//  ProxiFocus
+//
+//  Created by Daylen Mackey on 2024-12-19.
+//
+
+import SwiftUI
+
+@main
+struct ProxiFocusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ProxiFocus/ProxiFocusTests/ProxiFocusTests.swift
@@ -0,0 +1,17 @@
+//
+//  ProxiFocusTests.swift
+//  ProxiFocusTests
+//
+//  Created by Daylen Mackey on 2024-12-19.
+//
+
+import Testing
+@testable import ProxiFocus
+
+struct ProxiFocusTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
